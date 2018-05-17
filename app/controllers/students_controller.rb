91bcde9278571caa17ef8a6bdb8c3ef316dfa1a2
Chris Cardle <cdcardle@gmail.com>
2018-05-17 14:18:10 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -13,5 +13,6 @@ class StudentsController < ApplicationController
 
   def create
     Student.create(first_name: params[student][first_name], last_name: params[student][last_name])
+    redirect_to student_path
   end
 end
